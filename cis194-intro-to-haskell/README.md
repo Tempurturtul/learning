0bb2dacce14d903e04da61d89322bbf9fe6370b4
Tempurturtul <tempurturtul@gmail.com>
2016-06-19 14:59:41 -0400

    Update README and cis194 coursework

---
+++ cis194-intro-to-haskell/README.md
@@ -0,0 +1,11 @@
+# CIS194: Introduction to Haskell (Spring 2013)
+
+[Link to course.](http://www.seas.upenn.edu/~cis194/spring13/)
+
+**Instructor:** Brent Yorgey
+
+**Provider:** University of Pennsylvania - School of Engineering and Applied Science
+
+## Course Description
+
+> Haskell is a high-level, pure functional programming language with a strong static type system and elegant mathematical underpinnings, and is being increasingly used in industry by organizations such as Facebook, AT&T, and NASA. In the first 3/4 of the course, we will explore the joys of pure, lazy, typed functional programming in Haskell (warning: euphoric, mind-bending epiphanies may result!). The last 1/4 of the course will consist of case studies in fun and interesting applications of Haskell. Potential case study topics include automated randomized testing, software transactional memory, graphics generation, parser combinators, or others as time and interest allow. Evaluation will be based on class participation, weekly programming assignments, and an open-ended final project.
--- cis194-intro-to-haskell/wk01-intro/Main.hs
+++ cis194-intro-to-haskell/wk01-intro/Main.hs
@@ -5,6 +5,7 @@
    multi-line comment. -}
 
 
+
 -- Declarations and Variables
 
 -- x has type Int
@@ -17,6 +18,7 @@ y :: Int
 y = y + 1
 
 
+
 -- Basic Types
 
 -- Machine-sized integers. (Size limited by architecture: x86, x64.)
@@ -25,6 +27,7 @@ i = -78
 biggestInt, smallestInt :: Int
 biggestInt = maxBound
 smallestInt = minBound
+
 -- Arbitrary-precision integers. (Size limited only by memory.)
 n :: Integer
 n = 1234567890987654321987340982334987349872349874534
@@ -32,25 +35,31 @@ reallyBig :: Integer
 reallyBig = 2^(2^(2^(2^2)))
 numDigits :: Int
 numDigits = length (show reallyBig)
+
 -- Double-precision floating point.
 d1, d2 :: Double
 d1 = 4.58387
 d2 = 6.2831e-4
+
 -- Single-precision floating point.
 f :: Float
 f = 5.25
+
 -- Booleans.
 b1, b2 :: Bool
 b1 = True
 b2 = False
+
 -- Unicode characters.
 c1, c2, c3 :: Char
 c1 = 'x'
 c2 = 'Ø'
 c3 = 'ダ'
+
 -- Strings. (Lists of characters with special syntax.)
 s :: String
 s = "Hello, Haskell!"
+
 
 
 -- Arithmetic
@@ -74,6 +83,7 @@ ex09 = i `div` i
 ex10 = 12 `div` 5
 
 
+
 -- Boolean Logic
 
 ex11 = True && False
@@ -88,6 +98,7 @@ ex16 = "Haskell" > "C++"
    guards often used instead. -}
 
 
+
 -- Basic Functions
 
 -- Compute the sum of the integers from 1 to n.
@@ -95,9 +106,76 @@ sumtorial :: Integer -> Integer
 sumtorial 0 = 0
 sumtorial n = n + sumtorial (n-1)
 {- sumtorial :: Integer -> Integer says:
-     function sumtorial takes an Integer and yields another
+     Function sumtorial takes an Integer and yields another
      Integer. -}
 {- sumtorial 0 = 0 is a clause:
      Clauses are evaluated top to bottom with the first
      matching clause chosen. A variable like n matches
      anything. -}
+
+-- An example with guards.
+hailstone :: Integer -> Integer
+hailstone n
+  | n `mod` 2 == 0 = n `div` 2
+  | otherwise      = 3*n + 1
+{- | n `mod` 2 ... is an example of a guard associated with
+   a clause of the function definition. Guards are used
+   with Boolean expressions and evaluated top to bottom. If
+   none of the guards evaluate to True, matching continues
+   with the next clause. -}
+
+-- A more complex example.
+foo :: Integer -> Integer
+foo 0 = 16
+foo 1
+  | "Haskell" > "C++" = 3
+  | otherwise         = 4
+foo n
+  | n < 0             = 0
+  | n `mod` 17 == 2   = -43
+  | otherwise         = n + 3
+
+{- An example of abstracting out the evenness test in
+   hailstone. -}
+isEven :: Integer -> Bool
+isEven n
+  | n `mod` 2 == 0 = True
+  | otherwise      = False
+
+-- A simplification of the above function.
+betterIsEven :: Integer -> Bool
+betterIsEven n = mod n 2 == 0
+
+
+
+-- Pairs
+
+p :: (Int, Char)
+p = (3, 'x')
+
+-- Pattern matching to extract the elements of a pair.
+sumPair :: (Int,Int) -> Int
+sumPair (x,y) = x + y
+
+{- Haskell also has triples, quadruples, etc. but they
+   should not be used. -}
+
+
+
+-- Functions and Multiple Arguments
+
+sumThree :: Int -> Int -> Int -> Int
+sumThree x y z = x + y + z
+ex17 = sumThree 3 17 8
+
+{- NOTE: Function application has higher precedence than
+   any infix operators. -}
+-- Incorrect.
+-- sumThree 3 n+1 7
+-- Parses as: (sumThree 3 n) + (1 7)
+-- Correct.
+-- sumThree 3 (n+1) 7
+
+
+
+-- Lists
