3244514b89bdd1ba1f405adaee9ed780cd796559
Tempurturtul <tempurturtul@gmail.com>
2016-06-23 10:55:12 -0400

    Rename notes, add week 01 homework

---
+++ cis194-intro-to-haskell/wk01-intro/Homework01.hs
@@ -0,0 +1,13 @@
+{- Styleguide:
+     - DO use camelCase for function and variable names.
+     - DO use descriptive function names.
+     - DON'T use tabs.
+     - DO try to keep every line under 80 characters.
+     - DO give every top-level function a type signature. (It doesn't
+     hurt to give them to locally defined functions and constants as
+     well.)
+     - DO precede every top-level function by an explanatory comment.
+     - DO use -Wall (no warnings).
+     - DO use small functions and compose them to add complexity.
+     - DO make all your functions total (sensible results for every
+     input). -}
