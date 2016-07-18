ec3127d3b938c2fe62bc757a760a39714f71e78c
Tempurturtul <tempurturtul@gmail.com>
2016-07-18 15:48:38 -0400

    Finish homework 2 exercise 1

--- haskell/cis194-intro-to-haskell/wk02-algebraic-data-types/homework/LogAnalysis.hs
+++ haskell/cis194-intro-to-haskell/wk02-algebraic-data-types/homework/LogAnalysis.hs
@@ -4,3 +4,20 @@ module LogAnalysis where
 import Log
 
 -- Exercise 1 -------------------------------------------------------
+
+-- Parses a single log message. (Note: Fails if "read t" or "read l" doesn't produce an Int.)
+parseMessage :: String -> LogMessage
+parseMessage s = case (words s) of
+                   ("I":t:m)   -> LogMessage Info (read t) (unwords m)
+                   ("W":t:m)   -> LogMessage Warning (read t) (unwords m)
+                   ("E":l:t:m) -> LogMessage (Error (read l)) (read t) (unwords m)
+                   _           -> Unknown s
+
+-- Parses an entire log file.
+parse :: String -> [LogMessage]
+parse s = [parseMessage l | l <- lines s]
+
+-- Exercise 2 -------------------------------------------------------
+
+-- Inserts a new LogMessage into an existing MessageTree.
+-- insert :: LogMessage -> MessageTree -> MessageTree
