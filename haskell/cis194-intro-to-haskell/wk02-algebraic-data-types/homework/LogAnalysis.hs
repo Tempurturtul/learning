7f65a97d5ac2a380e34851a024db96bd1cd4b426
Tempurturtul <tempurturtul@gmail.com>
2016-07-19 10:52:15 -0400

    Finish week 2 homework

--- haskell/cis194-intro-to-haskell/wk02-algebraic-data-types/homework/LogAnalysis.hs
+++ haskell/cis194-intro-to-haskell/wk02-algebraic-data-types/homework/LogAnalysis.hs
@@ -5,7 +5,8 @@ import Log
 
 -- Exercise 1 -------------------------------------------------------
 
--- Parses a single log message. (Note: Fails if "read t" or "read l" doesn't produce an Int.)
+-- Parses a single log message.
+-- Note: Fails if "read t" or "read l" doesn't produce an Int.
 parseMessage :: String -> LogMessage
 parseMessage s = case (words s) of
                    ("I":t:m)   -> LogMessage Info (read t) (unwords m)
@@ -20,4 +21,39 @@ parse s = [parseMessage l | l <- lines s]
 -- Exercise 2 -------------------------------------------------------
 
 -- Inserts a new LogMessage into an existing MessageTree.
--- insert :: LogMessage -> MessageTree -> MessageTree
+-- Note: MessageTree input and output must be sorted by TimeStamp.
+-- Note: An Unknown LogMessage will return the tree unchanged.
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) tree = tree
+insert (LogMessage a b c) Leaf = Node Leaf (LogMessage a b c) Leaf
+insert (LogMessage a b c) (Node ltree (LogMessage x y z) rtree)
+  | y > b     = (Node (insert (LogMessage a b c) ltree) (LogMessage x y z) rtree)
+  | otherwise = (Node ltree (LogMessage x y z) (insert (LogMessage a b c) rtree))
+insert _ (Node _ (Unknown _) _) = error "MessageTree contains an Unknown LogMessage."
+
+-- Exercise 3 -------------------------------------------------------
+
+-- Builds a MessageTree.
+build :: [LogMessage] -> MessageTree
+build []     = Leaf
+build (x:xs) = insert x (build xs)
+
+-- Exercise 4 -------------------------------------------------------
+
+-- Perform an in-order traversal of the MessageTree.
+-- Note: There are better ways to sort a list, this is an exercise in recursive data structures.
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node ltree (LogMessage a b c) rtree) = inOrder ltree ++ [LogMessage a b c] ++ inOrder rtree
+inOrder (Node _ (Unknown _) _) = error "MessageTree contains an Unknown LogMessage."
+
+-- Exercise 5 -------------------------------------------------------
+
+-- Gets timestamp sorted messages for errors of >= 50 severity.
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong [] = []
+whatWentWrong xs = [b | (LogMessage (Error a) _ b) <- (inOrder (build xs)), a >= 50]
+
+-- Exercise 6 (Optional) --------------------------------------------
+
+-- TODO
