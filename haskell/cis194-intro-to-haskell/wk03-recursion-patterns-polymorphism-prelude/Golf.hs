c1c1f14b954c398d297cd0992e0bbf8f765d89ea
Tempurturtul <tempurturtul@gmail.com>
2016-07-24 15:44:25 -0400

    Finish homework 3 exercise 2

--- haskell/cis194-intro-to-haskell/wk03-recursion-patterns-polymorphism-prelude/Golf.hs
+++ haskell/cis194-intro-to-haskell/wk03-recursion-patterns-polymorphism-prelude/Golf.hs
@@ -13,7 +13,6 @@ skipsFrom n xs
 skips :: [a] -> [[a]]
 skips xs = skipsFrom 1 xs
 
-
 {- Explanatory Comment:
 
     The skips function simply calls skipsFrom with an n of 1 and xs
@@ -34,4 +33,25 @@ skips xs = skipsFrom 1 xs
 -- Exercise 2 -------------------------------------------------------
 
 -- Finds all local maxima and returns them in order.
--- localMaxima :: [Integer] -> [Integer]
+localMaxima :: [Integer] -> [Integer]
+localMaxima (x:y:z:zs)
+  | y > x && y > z = y : localMaxima (y:z:zs)
+  | otherwise      = localMaxima (y:z:zs)
+localMaxima _ = []
+
+{- Explanatory Comment:
+
+     If there are at least 3 Integers in the list:
+       If the 2nd Integer is greater than the 1st and 3rd:
+         Cons the 2nd Integer onto the result of the recursive call
+         using the list formed from the 2nd to the nth Integer.
+       If the 2nd Integer isn't greater than the 1st and 3rd:
+         Just return the result of the recursive call using the List
+         formed from the 2nd to the nth Integer.
+     If there aren't at least 3 Integers in the list:
+       Just return an empty list. -}
+
+-- Exercise 3 -------------------------------------------------------
+
+-- Takes a list from 0 to 9 and outputs a vertical histogram.
+-- histogram :: [Integer] -> String
