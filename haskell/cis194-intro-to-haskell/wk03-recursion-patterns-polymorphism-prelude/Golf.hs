b3a0620b538630b8951eea1e39f53fa1a577dd4b
Tempurturtul <tempurturtul@gmail.com>
2016-07-25 08:40:03 -0400

    Refactor week 3 homework

--- haskell/cis194-intro-to-haskell/wk03-recursion-patterns-polymorphism-prelude/Golf.hs
+++ haskell/cis194-intro-to-haskell/wk03-recursion-patterns-polymorphism-prelude/Golf.hs
@@ -3,53 +3,49 @@ module Golf where
 
 -- Exercise 1 -------------------------------------------------------
 
--- Gets lists of every nth element from n to length.
-skipsFrom :: Int -> [a] -> [[a]]
-skipsFrom n xs
-  | n < 1 || n > length xs = []
-  | otherwise              = [x | (x,i) <- (zip xs [1..length xs]), mod i n == 0] : skipsFrom (n+1) xs
+-- Gets every x from a list of (x, m) pairs where mod m n == 0.
+skip :: [(a, Int)] -> Int -> [a]
+skip xs n
+  | n < 1     = []
+  | otherwise = [x | (x, m) <- xs, mod m n == 0]
 
 -- Gets lists of every nth element from 1 to length.
 skips :: [a] -> [[a]]
-skips xs = skipsFrom 1 xs
+skips [] = []
+skips xs = map (skip (zip xs [1..length xs])) [1..length xs]
 
 {- Explanatory Comment:
 
-    The skips function simply calls skipsFrom with an n of 1 and xs
-    of the unaltered list.
-
-    The skipsFrom function handles the case of n < 1 and the base
-    case of n > length xs by returning an empty list. For all other
-    cases, it forms a new list from xs by zipping each element x with
-    an Int m representing the element's index in xs + 1, then uses
-    list comprehension to filter the list to include only elements
-    where mod m n == 0 (that is, the mth element is divisible by n),
-    and then uses pattern-matching to extract x from the (x, m) pair.
-
-    This produces a list of every nth element from xs, which is then
-    cons'd onto the result of the recursive call to skipsFrom with an
-    incremented n and the original xs. -}
+   First, the skip function:
+     The skip function takes a list of pairs (x,m) and an Int n,
+     filters that list using list comprehension to only include pairs
+     where m is divisible by n, then uses pattern matching in the
+     list comprehension to output a list of only x's.
+   The skips function:
+     The skips function maps the skip function, giving it a list of
+     xs zipped with 1 to length xs, over a list of Ints 1 to length
+     xs. The result is that for the Ints 1 to length xs, representing
+     each element in xs, the skip function is applied to the list of
+     pairs (x, m), where each x corresponds to the element in xs and
+     each m corresponds to the element's index + 1. The first
+     argument to skip remains the same and the second increments.
+     Each iteration of map produces a list of xs, with the end result
+     being a list of lists of xs. -}
 
 -- Exercise 2 -------------------------------------------------------
 
 -- Finds all local maxima and returns them in order.
 localMaxima :: [Integer] -> [Integer]
-localMaxima (x:y:z:zs)
-  | y > x && y > z = y : localMaxima (y:z:zs)
-  | otherwise      = localMaxima (y:z:zs)
-localMaxima _ = []
+localMaxima (x:y:zs) = [b | (a,b,c) <- (zip3 (x:y:zs) (y:zs) zs), a < b && b > c]
+localMaxima _        = []
 
 {- Explanatory Comment:
 
-     If there are at least 3 Integers in the list:
-       If the 2nd Integer is greater than the 1st and 3rd:
-         Cons the 2nd Integer onto the result of the recursive call
-         using the list formed from the 2nd to the nth Integer.
-       If the 2nd Integer isn't greater than the 1st and 3rd:
-         Just return the result of the recursive call using the List
-         formed from the 2nd to the nth Integer.
-     If there aren't at least 3 Integers in the list:
-       Just return an empty list. -}
+   Use zip3 with the full list, and then the list minus the head, and
+   then that list minus the head to get only triples from the list.
+   Then use list comprehension and pattern-matching to compare the
+   middle Integer from the triple to its neighbors. If it's greater
+   than its neighbors, include it in the output list. -}
 
 -- Exercise 3 -------------------------------------------------------
 
