b6c1b060c183fed971d710b41a16ebff520cb4b4
Tempurturtul <tempurturtul@gmail.com>
2016-08-16 14:17:32 -0400

    Fix homework 4 exercise 1

--- haskell/cis194-intro-to-haskell/04-higher-order-and-type-inference/Homework.hs
+++ haskell/cis194-intro-to-haskell/04-higher-order-and-type-inference/Homework.hs
@@ -3,27 +3,26 @@ module Homework where
 
 -- Exercise 1 -------------------------------------------------------
 
-{- Reimplement following functions in a more idiomatic Haskell style.
-
-  fun1 :: [Integer] -> Integer
-  fun1 []       = 1
-  fun1 (x:xs)
-    | even x    = (x - 2) * fun1 xs
-    | otherwise = fun1 xs
-
-  fun2 :: Integer -> Integer
-  fun2 1 = 0
-  fun2 n | even n    = n + fun2 (n `div` 2)
-         | otherwise = fun2 (3 * n + 1)
--}
-
 fun1 :: [Integer] -> Integer
 fun1 = product . map (\x -> x - 2) . filter even
 
 fun2 :: Integer -> Integer
-fun2 = sum . takeWhile (/= 1) . iterate (\n -> if even n
-                                               then n `div` 2
-                                               else 3 * n + 1)
+fun2 = sum . filter even . takeWhile (/= 1) . iterate (\n -> if even n
+                                                             then n `div` 2
+                                                             else 3 * n + 1)
+
+-- Originals for comparison.
+
+fun1' :: [Integer] -> Integer
+fun1' []       = 1
+fun1' (x:xs)
+  | even x    = (x - 2) * fun1' xs
+  | otherwise = fun1' xs
+
+fun2' :: Integer -> Integer
+fun2' 1 = 0
+fun2' n | even n    = n + fun2' (n `div` 2)
+        | otherwise = fun2' (3 * n + 1)
 
 -- Exercise 2 -------------------------------------------------------
 
