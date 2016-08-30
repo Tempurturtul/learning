95ee2fdca8d53b89dc2af411a4ebad45131e48f5
Tempurturtul <tempurturtul@gmail.com>
2016-08-30 10:34:48 -0400

    Add week 6 homework 1

--- haskell/cis194-intro-to-haskell/06-lazy-evaluation/Fibonacci.hs
+++ haskell/cis194-intro-to-haskell/06-lazy-evaluation/Fibonacci.hs
@@ -2,3 +2,13 @@
 module Fibonacci where
 
 -- Exercise 1 -------------------------------------------------------
+
+-- Computes the specified Fibonacci number.
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = (+) (fib $ n-1) (fib $ n-2)
+
+-- Defines the infinite list of Fibonacci numbers.
+fibs1 :: [Integer]
+fibs1 = [fib x | x <- [0..]]
