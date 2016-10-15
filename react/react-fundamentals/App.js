dbddac4a3ce25f63b6bb01f91d96b2c6e70c2b2d
Tempurturtul <tempurturtul@gmail.com>
2016-10-14 20:03:27 -0400

    Add react-fundamentals 02 - simple component

--- react/react-fundamentals/App.js
+++ react/react-fundamentals/App.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return <div>Hello</div>
+    // Using JSX:
+    return <h1>Hello World</h1>
+
+    // Alternatively without using JSX:
+    // return React.createElement('h1', null, 'Hello Guys'); // element, props, content
   }
 }
 
-export default App
+// Stateless function component.
+// const App = () => <h1>Hello Eggheads</h1>
+
+export default App;
