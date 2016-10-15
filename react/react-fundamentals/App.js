6c427f30509fc4cd8f2cc3ff3c1b27ba27c10714
Tempurturtul <tempurturtul@gmail.com>
2016-10-14 20:20:48 -0400

    Add react-fundamentals 05 - state

--- react/react-fundamentals/App.js
+++ react/react-fundamentals/App.js
@@ -1,27 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      txt: 'this is the state txt'
+    }
+  }
+
+  update(e) {
+    this.setState({txt: e.target.value})
+  }
+
   render() {
-    let txt = this.props.txt;
-    return <h1>{txt}</h1>;
+    return (
+      <div>
+        <input type="text" onChange={this.update.bind(this)} />
+        <h1>{this.state.txt}</h1>
+      </div>
+    );
   }
 }
-
-// Identifies the types of properties expected on the component.
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
-
-// Default properties.
-App.defaultProps = {
-  txt: 'this is the default txt'
-};
-
-ReactDOM.render(
-  <App cat={5} txt="this is the prop's value" />,
-  document.getElementById('app')
-);
 
 export default App;
