2b3cb5aae099c32de72c8e48c7b5940451c3c91a
Tempurturtul <tempurturtul@gmail.com>
2016-10-14 20:16:48 -0400

    Add react-fundamentals 04 - properties

--- react/react-fundamentals/App.js
+++ react/react-fundamentals/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render() {
-    /* If returning multiple elements, must wrap them in a single node.
-     * The parantheses are optional, but if excluded the first tag must
-     * immediately follow the return. */
-    return (
-      <div>
-        <h1>Hello World</h1>
-        <b>Bold</b>
-      </div>
-    );
+    let txt = this.props.txt;
+    return <h1>{txt}</h1>;
   }
 }
+
+// Identifies the types of properties expected on the component.
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+}
+
+// Default properties.
+App.defaultProps = {
+  txt: 'this is the default txt'
+};
+
+ReactDOM.render(
+  <App cat={5} txt="this is the prop's value" />,
+  document.getElementById('app')
+);
 
 export default App;
