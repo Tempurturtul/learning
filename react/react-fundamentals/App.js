519d405118e832365d7bbb319a97b42a29a82a4b
Tempurturtul <tempurturtul@gmail.com>
2016-10-20 09:37:01 -0400

    Add react-fundamentals 15 - JSX live compiler

--- react/react-fundamentals/App.js
+++ react/react-fundamentals/App.js
@@ -4,36 +4,45 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      data: [
-        {id: 1, name: "Simon Bailey"},
-        {id: 2, name: "Thomas Buleson"},
-        {id: 3, name: "Will Button"},
-        {id: 4, name: "Ben Clinkinbeard"}
-      ]
+      input: '/* add your jsx here*/',
+      output: '',
+      err: ''
     };
+    this.update = this.update.bind(this);
+  }
+
+  update(e) {
+    let code = e.target.value;
+    try {
+      this.setState({
+        output: babel.transform(code, {
+          stage: 0,
+          loose: 'all'
+        }).code,
+        err: ''
+      });
+    }
+    catch(err) {
+      this.setState({err: err.message});
+    }
   }
 
   render() {
-    let rows = this.state.data.map(person => {
-      // Siblings require unique keys.
-      return <PersonRow key={person.id} data={person} />;
-    });
-
     return (
-      <table>
-        <tbody>{rows}</tbody>
-      </table>
+      <div>
+        <header>{this.state.err}</header>
+        <div className="contaner">
+          <textarea
+            onChange={this.update}
+            defaultValue={this.state.input}>
+          </textarea>
+          <pre>
+            {this.state.output}
+          </pre>
+        </div>
+      </div>
     );
   }
-}
-
-const PersonRow = (props) => {
-  return (
-    <tr>
-      <td>{props.data.id}</td>
-      <td>{props.data.name}</td>
-    </tr>
-  );
 }
 
 export default App;
