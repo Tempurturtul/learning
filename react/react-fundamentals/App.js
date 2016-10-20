e44fad1afc60ec1b4bd2cd1d796389a9bc1d7414
Tempurturtul <tempurturtul@gmail.com>
2016-10-20 08:12:07 -0400

    Add react-fundamentals 07 - refs

--- react/react-fundamentals/App.js
+++ react/react-fundamentals/App.js
@@ -1,34 +1,53 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {txt: ''}
+    this.state = {
+      red: 0,
+      green: 0,
+      blue: 0
+    }
     this.update = this.update.bind(this);
   }
 
   update(e) {
-    this.setState({txt: e.target.value})
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
+    })
   }
 
   render() {
     return (
       <div>
-        <Widget txt={this.state.txt} update={this.update} />
-        <Widget txt={this.state.txt} update={this.update} />
-        <Widget txt={this.state.txt} update={this.update} />
+        <Slider ref="red" update={this.update} />
+        {this.state.red}
+        <br />
+        <Slider ref="green" update={this.update} />
+        {this.state.green}
+        <br />
+        <Slider ref="blue" update={this.update} />
+        {this.state.blue}
+        <br />
       </div>
     );
   }
 }
 
-const Widget = (props) => {
-  return (
-    <div>
-      <input type="text" onChange={props.update} />
-      <h1>{props.txt}</h1>
-    </div>
-  );
-};
+class Slider extends React.Component {
+  render() {
+    return (
+      <div>
+        <input ref="inp" type="range"
+          min="0"
+          max="255"
+          onChange={this.props.update}/>
+      </div>
+    );
+  }
+}
 
 export default App;
