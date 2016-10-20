6bce72e49ff27fd1d746dbdbfdc6dca5b33bb353
Tempurturtul <tempurturtul@gmail.com>
2016-10-20 09:17:55 -0400

    Add react-fundamentals 14 - dynamic components

--- react/react-fundamentals/App.js
+++ react/react-fundamentals/App.js
@@ -1,73 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      red: 0
+      data: [
+        {id: 1, name: "Simon Bailey"},
+        {id: 2, name: "Thomas Buleson"},
+        {id: 3, name: "Will Button"},
+        {id: 4, name: "Ben Clinkinbeard"}
+      ]
     };
-    this.update = this.update.bind(this);
   }
 
-  update(e) {
-    this.setState({
-      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+  render() {
+    let rows = this.state.data.map(person => {
+      // Siblings require unique keys.
+      return <PersonRow key={person.id} data={person} />;
     });
-  }
 
-  render() {
     return (
-      <div>
-        <NumInput
-          ref="red"
-          min={0}
-          max={255}
-          step={0.01}
-          val={+this.state.red}
-          label="Red"
-          update={this.update} />
-      </div>
+      <table>
+        <tbody>{rows}</tbody>
+      </table>
     );
   }
 }
 
-class NumInput extends React.Component {
-  render() {
-    let label = this.props.label !== '' ?
-      <label>{this.props.label} - {this.props.val}</label> : ''
-    return (
-      <div>
-        <input ref="inp"
-          type={this.props.type}
-          min={this.props.min}
-          max={this.props.max}
-          step={this.props.step}
-          defaultValue={this.props.val}
-          onChange={this.props.update} />
-        {label}
-      </div>
-    );
-  }
+const PersonRow = (props) => {
+  return (
+    <tr>
+      <td>{props.data.id}</td>
+      <td>{props.data.name}</td>
+    </tr>
+  );
 }
-
-NumInput.propTypes = {
-  min: React.PropTypes.number,
-  max: React.PropTypes.number,
-  step: React.PropTypes.number,
-  val: React.PropTypes.number,
-  label: React.PropTypes.string,
-  update: React.PropTypes.func.isRequired,
-  type: React.PropTypes.oneOf(['number', 'range'])
-};
-
-NumInput.defaultProps = {
-  min: 0,
-  max: 0,
-  step: 1,
-  val: 0,
-  label: '',
-  type: 'range'
-};
 
 export default App;
