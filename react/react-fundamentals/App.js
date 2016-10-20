50eef864d8975ecdb5a64cad5745804c8645c766
Tempurturtul <tempurturtul@gmail.com>
2016-10-20 08:35:50 -0400

    Add react-fundamentals 09 - lifecycle methods

--- react/react-fundamentals/App.js
+++ react/react-fundamentals/App.js
@@ -1,19 +1,57 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { val: 0 };
+    this.update = this.update.bind(this);
+  }
+
+  update() {
+    this.setState({val: this.state.val + 1});
+  }
+
+  componentWillMount() {
+    console.log('mounting');
+  }
+
+  render() {
+    console.log('rendering!');
+    return <button onClick={this.update}>{this.state.val}</button>
+  }
+
+  componentDidMount() {
+    console.log('mounted');
+  }
+
+  componentWillUnmount() {
+    console.log('bye!');
+  }
+}
+
+class Wrapper extends React.Component {
+  constructor() {
+    super();
+  }
+
+  mount() {
+    ReactDOM.render(<App />, document.getElementById('a'));
+  }
+
+  unmount() {
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'));
+  }
+
   render() {
     return (
-      <Button>I <Heart /> React</Button>
+      <div>
+        <button onClick={this.mount.bind(this)}>Mount</button>
+        <button onClick={this.unmount.bind(this)}>Unmount</button>
+        <div id="a"></div>
+      </div>
     );
   }
 }
 
-class Button extends React.Component {
-  render() {
-    return <button>{this.props.children}</button>
-  }
-}
-
-const Heart = () => <span>&lt;3</span>
-
-export default App;
+export default Wrapper;
