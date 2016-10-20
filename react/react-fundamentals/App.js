06a7c74de9d36ff5789836c091a8679a4a5d74c3
Tempurturtul <tempurturtul@gmail.com>
2016-10-20 09:58:00 -0400

    Add react-fundamentals 16 - understanding JSX

--- react/react-fundamentals/App.js
+++ react/react-fundamentals/App.js
@@ -4,7 +4,27 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      input: '/* add your jsx here*/',
+      input: `\
+const App = (props) => {
+  var myStyle = {
+    backgroundColor: '#000',
+    height: 10 // px added by React.
+  };
+
+  return (
+    <div style={myStyle}>
+      <a href="#"
+        notrendered="x"
+        data-rendered="y"
+        onClick={update}>
+        {/* this is a comment */}
+        this is the text
+        {i>1 ? 'More than one' : 'one'}
+        {i>1 && 'More than one'}
+      </a>
+    </div>
+  );
+};`,
       output: '',
       err: ''
     };
@@ -31,7 +51,7 @@ class App extends React.Component {
     return (
       <div>
         <header>{this.state.err}</header>
-        <div className="contaner">
+        <div className="container">
           <textarea
             onChange={this.update}
             defaultValue={this.state.input}>
