558d48d39dcc60fb6b5f838804f113145e38b52f
Tempurturtul <tempurturtul@gmail.com>
2016-10-20 08:42:53 -0400

    Add react-fundamentals 10 - lifecycle method uses

--- react/react-fundamentals/App.js
+++ react/react-fundamentals/App.js
@@ -14,19 +14,30 @@ class App extends React.Component {
 
   componentWillMount() {
     console.log('mounting');
+    // Change state before component mounts.
+    this.setState({m: 2});
   }
 
   render() {
     console.log('rendering!');
-    return <button onClick={this.update}>{this.state.val}</button>
+    return (
+      <button onClick={this.update}>
+        {this.state.val * this.state.m}
+      </button>
+    );
   }
 
   componentDidMount() {
     console.log('mounted');
+    // Can access the DOM at this stage.
+    console.log(ReactDOM.findDOMNode(this));
+    this.inc = setInterval(this.update, 500);
   }
 
   componentWillUnmount() {
     console.log('bye!');
+    // Clean up.
+    clearInterval(this.inc);
   }
 }
 
