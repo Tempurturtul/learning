2ed88826b911e51743381b3dc3bd7c79b1fb5ce4
Tempurturtul <tempurturtul@gmail.com>
2016-10-20 08:01:16 -0400

    Add react-fundamentals 06 - composite components

--- react/react-fundamentals/App.js
+++ react/react-fundamentals/App.js
@@ -3,9 +3,8 @@ import React from 'react';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      txt: 'this is the state txt'
-    }
+    this.state = {txt: ''}
+    this.update = this.update.bind(this);
   }
 
   update(e) {
@@ -15,11 +14,21 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <input type="text" onChange={this.update.bind(this)} />
-        <h1>{this.state.txt}</h1>
+        <Widget txt={this.state.txt} update={this.update} />
+        <Widget txt={this.state.txt} update={this.update} />
+        <Widget txt={this.state.txt} update={this.update} />
       </div>
     );
   }
 }
+
+const Widget = (props) => {
+  return (
+    <div>
+      <input type="text" onChange={props.update} />
+      <h1>{props.txt}</h1>
+    </div>
+  );
+};
 
 export default App;
