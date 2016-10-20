b093ad29dfd4a54210324920be42cf8105d6fb1b
Tempurturtul <tempurturtul@gmail.com>
2016-10-20 15:27:23 -0400

    Update flux-todo-list - store (simplified)

--- react/flux-todo-list/app/js/stores/TodoStore.js
+++ react/flux-todo-list/app/js/stores/TodoStore.js
@@ -1,14 +1,3 @@
-/*
- * Copyright (c) 2014, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- *
- * TodoStore
- */
-
 // Private data structure _todos contains all individual todo items.
 // Store's callback is registered with the dispatcher.
 
@@ -26,36 +15,13 @@ var _todos = {}; // Collection of todo items.
  * @param {string} text - The content of the TODO.
  */
 function create(text) {
-  // Hand waving here -- not showing how this interacts with XHR or persistent
-  // server-side storage.
-  // Using the current timestamp + random number in place of a real id.
-  var id = (+new Date() + Math.floor(Math.random() * 999999)).toString(36);
+  // Using the current timestamp in place of a real id.
+  var id = Date.now();
   _todos[id] = {
     id: id,
     complete: false,
     text: text
   };
-}
-
-/**
- * Update a TODO item.
- * @param {string} id
- * @param {object} updates - An object literal containing only the data to be
- * updated.
- */
-function update(id, updates) {
-  _todos[id] = assign({}, _todos[id], updates);
-}
-
-/**
- * Update all of the TODO items with the same object.
- * @param {object} updates - An object literal containing only the data to be
- * updated.
- */
-function updateAll(updates) {
-  for (var id in _todos) {
-    update(id, updates);
-  }
 }
 
 /**
@@ -66,31 +32,7 @@ function destroy(id) {
   delete _todos[id];
 }
 
-/**
- * Delete all the completed TODO items.
- */
-function destroyCompleted() {
-  for (var id in _todos) {
-    if (_todos[id].complete) {
-      destroy(id);
-    }
-  }
-}
-
 var TodoStore = assign({}, EventEmitter.prototype, {
-
-  /**
-   * Tests whether all the remaining TODO items are marked as completed.
-   * @return {boolean}
-   */
-  areAllComplete: function() {
-    for (var id in _todos) {
-      if (!_todos[id].complete) {
-        return false;
-      }
-    }
-    return true;
-  },
 
   /**
    * Get the entire collection of TODOs.
@@ -116,62 +58,34 @@ var TodoStore = assign({}, EventEmitter.prototype, {
    */
   removeChangeListener: function(callback) {
     this.removeListener(CHANGE_EVENT, callback);
-  }
-});
+  },
 
-// Register callback to handle all updates
-AppDispatcher.register(function(action) {
-  var text;
+  dispatcherIndex: AppDispatcher.register(function(payload) {
+    var action = payload.action;
+    var text;
 
-  switch(action.actionType) {
-    case TodoConstants.TODO_CREATE:
-      text = action.text.trim();
-      if (text !== '') {
-        create(text);
+    switch(action.actionType) {
+      case TodoConstants.TODO_CREATE:
+        text = action.text.trim();
+        if (text !== '') {
+          create(text);
+          TodoStore.emitChange();
+        }
+        break;
+
+      case TodoConstants.TODO_DESTROY:
+        destroy(action.id);
         TodoStore.emitChange();
-      }
-      break;
+        break;
 
-    case TodoConstants.TODO_TOGGLE_COMPLETE_ALL:
-      if (TodoStore.areAllComplete()) {
-        updateAll({complete: false});
-      } else {
-        updateAll({complete: true});
-      }
-      TodoStore.emitChange();
-      break;
+      // Add more cases for other actionTypes, like TODO_UPDATE, etc.
 
-    case TodoConstants.TODO_UNDO_COMPLETE:
-      update(action.id, {complete: false});
-      TodoStore.emitChange();
-      break;
+      default:
+        // no op
+    }
 
-    case TodoConstants.TODO_COMPLETE:
-      update(action.id, {complete: true});
-      TodoStore.emitChange();
-      break;
-
-    case TodoConstants.TODO_UPDATE_TEXT:
-      text = action.text.trim();
-      if (text !== '') {
-        update(action.id, {text: text});
-        TodoStore.emitChange();
-      }
-      break;
-
-    case TodoConstants.TODO_DESTROY:
-      destroy(action.id);
-      TodoStore.emitChange();
-      break;
-
-    case TodoConstants.TODO_DESTROY_COMPLETED:
-      destroyCompleted();
-      TodoStore.emitChange();
-      break;
-
-    default:
-      // no op
-  }
+    return true; // No errors. Needed by promise in Dispatcher.
+  })
 });
 
 module.exports = TodoStore;
