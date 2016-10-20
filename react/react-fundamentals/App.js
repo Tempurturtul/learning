d0f609579f16f4f9e42b1bcc845977c5575b3d8a
Tempurturtul <tempurturtul@gmail.com>
2016-10-20 08:25:44 -0400

    Add react-fundamentals 08 - this.props.children

--- react/react-fundamentals/App.js
+++ react/react-fundamentals/App.js
@@ -1,53 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      red: 0,
-      green: 0,
-      blue: 0
-    }
-    this.update = this.update.bind(this);
-  }
-
-  update(e) {
-    this.setState({
-      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
-      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
-      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
-    })
-  }
-
   render() {
     return (
-      <div>
-        <Slider ref="red" update={this.update} />
-        {this.state.red}
-        <br />
-        <Slider ref="green" update={this.update} />
-        {this.state.green}
-        <br />
-        <Slider ref="blue" update={this.update} />
-        {this.state.blue}
-        <br />
-      </div>
+      <Button>I <Heart /> React</Button>
     );
   }
 }
 
-class Slider extends React.Component {
+class Button extends React.Component {
   render() {
-    return (
-      <div>
-        <input ref="inp" type="range"
-          min="0"
-          max="255"
-          onChange={this.props.update}/>
-      </div>
-    );
+    return <button>{this.props.children}</button>
   }
 }
+
+const Heart = () => <span>&lt;3</span>
 
 export default App;
