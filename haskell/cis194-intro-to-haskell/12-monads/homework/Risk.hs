f37c2bb53f6d4435371e941ca7d8ae2f47f2b38d
Tempurturtul <tempurturtul@gmail.com>
2017-02-09 15:27:48 -0500

    Finish week 12 exercise 1

--- haskell/cis194-intro-to-haskell/12-monads/homework/Risk.hs
+++ haskell/cis194-intro-to-haskell/12-monads/homework/Risk.hs
@@ -35,53 +35,72 @@ data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
 
 -- Exercise 2 -------------------------------------------------------
 
-instance Show Battlefield where
-  show bf = "Attackers: " ++ show (attackers bf) ++ " Defenders: " ++ show (defenders bf)
-
-instance Num Battlefield where
-  (+) a b     = Battlefield (attackers a + attackers b) (defenders a + defenders b)
-  (-) a b     = Battlefield (attackers a - attackers b) (defenders a - defenders b)
-
-instance Eq Battlefield where
-  (==) a b = ((attackers a) == (attackers b)) && ((defenders a) == (defenders b))
-
 -- Gets multiple dice rolls.
 dice :: Int -> Rand StdGen [DieValue]
 dice n = sequence (replicate n die)
 
--- Gets a battlefield comprised only of units capable of engaging in a single battle.
-getBattle :: Battlefield -> Battlefield
-getBattle bf = Battlefield atk def
-  where
-    atk = (min (attackers bf - 1) 3)
-    def = (min (defenders bf) 2)
+-- Gets units capable of engaging in a battle.
+getBattlers :: Battlefield -> (Army, Army)
+getBattlers bf = (min (attackers bf - 1) 3, min (defenders bf) 2)
 
--- Resolves a single battle.
-resolve :: Battlefield -> [DieValue] -> Battlefield
-resolve bf xs = bf - casualties
+-- Gets multiple dice rolls.
+getRolls :: Int -> Rand StdGen [DieValue]
+getRolls n = sequence (replicate n die)
+
+-- Resolves a battle.
+resolve :: (Army, Army) -> [DieValue] -> (Army, Army)
+resolve (atk, def) rolls = (atk - failed, def - successful)
   where
-    casualties = Battlefield failedAttacks (min successfulAttacks (defenders bf))
-    failedAttacks = length . filter (uncurry (<=)) $ zip atks defs
-    successfulAttacks = length . filter (uncurry (>)) $ zip atks defs
-    atks = reverse . sort $ take (attackers bf) xs
-    defs = reverse . sort . take (defenders bf) $ drop (length atks) xs
+    failed      = length . filter (uncurry (<=)) $ pairedRolls
+    successful  = length . filter (uncurry (>)) $ pairedRolls
+    pairedRolls = zip atkRolls defRolls
+    atkRolls    = reverse . sort $ take atk rolls
+    defRolls    = reverse . sort $ take def (drop atk rolls)
+
+-- Updates a battlefield to reflect the results of a battle.
+update :: Battlefield -> (Army, Army) -> Battlefield
+update bf result = Battlefield updatedAttackers updatedDefenders
+  where
+    updatedAttackers = (attackers bf) - (fst battlers) + (fst result)
+    updatedDefenders = (defenders bf) - (snd battlers) + (snd result)
+    battlers         = getBattlers bf
 
 -- Simulates a single battle. Assumes maximum number of units are used.
 battle :: Battlefield -> Rand StdGen Battlefield
-battle bf = dice 5 >>= (\xs -> return (resolve battlers xs)) >>= newField
+battle bf = rolls >>= (\rs -> return (resolve battlers rs)) >>= (\armies -> return (update bf armies))
   where
-    battlers = getBattle bf
-    newField = (\resolved -> return (bf - battlers + resolved))
+    battlers = getBattlers bf
+    rolls = dice (uncurry (+) battlers)
 
--- Simplest.
-test01 = (resolve (Battlefield 1 1) ([DV 6] ++ [DV 1])) == (Battlefield 1 0)
--- Tied rolls.
-test02 = (resolve (Battlefield 1 1) ([DV 6] ++ [DV 6])) == (Battlefield 0 1)
--- More attackers.
-test03 = (resolve (Battlefield 2 1) ([DV 6, DV 6] ++ [DV 1])) == (Battlefield 2 0)
--- More defenders.
-test04 = (resolve (Battlefield 1 2) ([DV 6] ++ [DV 1, DV 1])) == (Battlefield 1 1)
--- Realistic.
-test05 = (resolve (Battlefield 3 2) ([DV 2, DV 5, DV 1] ++ [DV 3, DV 4])) == (Battlefield 2 1)
+-- Tests ------------------------------------------------------------
 
-tests = [test01, test02, test03, test04, test05]
+getBattlers01 = (getBattlers (Battlefield 1 0)) == (0, 0)
+getBattlers02 = (getBattlers (Battlefield 8 8)) == (3, 2)
+getBattlers03 = (getBattlers (Battlefield 2 1)) == (1, 1)
+getBattlers04 = (getBattlers (Battlefield 4 2)) == (3, 2)
+getBattlersTests = [getBattlers01, getBattlers02, getBattlers03, getBattlers04]
+
+resolve01 = (resolve (1, 1) ([3] ++ [6])) == (0, 1)
+resolve02 = (resolve (2, 2) ([3,3] ++ [6,1])) == (1, 1)
+resolve03 = (resolve (2, 2) ([3,3] ++ [3,3])) == (0, 2)
+resolve04 = (resolve (3, 2) ([3,3,3] ++ [1,1])) == (3, 0)
+resolve05 = (resolve (3, 2) ([1,1,1] ++ [3,3])) == (1, 2)
+resolve06 = (resolve (1, 2) ([1] ++ [3,3])) == (0, 2)
+resolve07 = (resolve (1, 2) ([6] ++ [3,3])) == (1, 1)
+resolveTests = [resolve01, resolve02, resolve03, resolve04, resolve05, resolve06, resolve07]
+
+instance Eq Battlefield where
+  (==) a b = (attackers a == attackers b) && (defenders a == defenders b)
+
+update01 = (update (Battlefield 5 5) (3, 0)) == Battlefield 5 3
+update02 = (update (Battlefield 2 1) (0, 1)) == Battlefield 1 1
+update03 = (update (Battlefield 3 1) (2, 0)) == Battlefield 3 0
+updateTests = [update01, update02, update03]
+
+instance Show Battlefield where
+  show bf = "Attackers: " ++ show (attackers bf) ++ ", Defenders: " ++ show (defenders bf)
+
+main :: IO()
+main = do
+  b <- evalRandIO (battle (Battlefield 8 8))
+  print b
